831ddb6a69657ec1787e2f9837d482ba88ab9ba4
Joan Cerepi <cyberstriker27@proton.me>
2024-12-10 21:20:11 -0700

    added login and registration views with functionality and validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,8 +3,18 @@ const express = require('express')
 const router = express.Router()
 const utilities = require('../utilities/')
 const accountController = require('../controllers/accountController')
+const regValidate = require('../utilities/account-validation')
 
-// Account routes
-router.get('/login', utilities.handleErrors(accountController))
+// Account get routes
+router.get('/login', utilities.handleErrors(accountController.buildLogin))
+router.get('/register', utilities.handleErrors(accountController.buildRegister))
+
+// Account post routes
+router.post(
+    '/register',
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,36 +7,61 @@
  *************************/
 
 // Required Packages
-const express = require("express");
-const env = require("dotenv").config();
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const utilities = require('./utilities/');
+const express = require("express")
+const env = require("dotenv").config()
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const utilities = require('./utilities/')
+const session = require('express-session')
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 // Required route files
-const static = require("./routes/static");
-const inventoryRoute = require('./routes/inventoryRoute');
+const static = require("./routes/static")
+const inventoryRoute = require('./routes/inventoryRoute')
+const accountRoute = require('./routes/accountRoute')
 
 // Required controller files
-const baseController = require('./controllers/baseController');
+const baseController = require('./controllers/baseController')
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-app.set('layout', './layouts/layout'); // not at views root
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set('layout', './layouts/layout') // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static);
-
-// Index route
-app.get('/', utilities.handleErrors(baseController.buildHome));
-
-// Inventory route
-app.use("/inv", inventoryRoute);
+app.use(static)
+app.get('/', utilities.handleErrors(baseController.buildHome))
+app.use("/inv", inventoryRoute)
+app.use('/account', accountRoute)
 
 // Intentional Error 500
 app.use('/throwError', baseController.throwError)
@@ -65,12 +90,12 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
+  console.log(`app listening on ${host}:${port}`)
 });
